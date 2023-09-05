c7c4b2af90eedac1c9299255caa32185d0859762
Vaishak <mvaishak02@gmail.com>
2023-09-05 20:19:51 +0530

    mid

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:["ssorhpggrryvoliilzce.supabase.co"]
+    }
+}
 
 module.exports = nextConfig
